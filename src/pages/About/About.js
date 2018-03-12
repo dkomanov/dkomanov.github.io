a683cb6f55333dd811ff739d0a2516c759088cc8
Dmitry Komanov <dmitryk@wix.com>
2018-03-13 00:05:06 +0200

    Revert "Extract code to StaticPage"
    This reverts commit 8d7e16b6ea3fa73b07678bfbd5dc5e2973143400.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,4 +1,14 @@
-import {StaticPage} from '../../components';
+import React from 'react';
+import {MainPage, Markdown} from '../../components';
 import {AboutMarkdown} from '../../content';
+import {Config} from '../../util';
 
-export default StaticPage('img/about-cover.jpg', AboutMarkdown);
+export default class About extends React.Component {
+  render() {
+    return (
+      <MainPage teaserUrl={`${Config.baseUrl}img/about-cover.jpg`}>
+        <Markdown source={AboutMarkdown}/>
+      </MainPage>
+    );
+  }
+}
