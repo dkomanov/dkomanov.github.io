42033576023a11310c1023e9b110b710e1bee638
Dmitry Komanov <dmitryk@wix.com>
2019-08-02 13:13:33 +0300

    Add redirects for old urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -137,5 +137,6 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-client-side-redirect`,
   ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,12 +2,13 @@ const path = require(`path`);
 const {createFilePath} = require(`gatsby-source-filesystem`);
 
 exports.createPages = async ({graphql, actions}) => {
-  const {createPage} = actions;
+  const {createPage, createRedirect} = actions;
 
   createBlogByTagPages(createPage, await queryTags(graphql));
   createBlogPages(createPage, await queryMarkdowns(graphql, 'blog'));
   createStaticPages(createPage, await queryMarkdowns(graphql, 'static'));
   createWhatIReadPages(createPage, await queryMonths(graphql));
+  createAllOldRedirects(createRedirect);
 };
 
 exports.onCreateNode = ({node, actions, getNode}) => {
@@ -185,7 +186,7 @@ const queryMonths = async (graphql) => {
     return {
       id: edge.node.id,
       path: index === 0 ? '/what-i-read' : `/what-i-read/${month}`,
-      month
+      month,
     };
   });
 };
@@ -204,3 +205,30 @@ const createWhatIReadPages = (createPage, months) => {
     });
   });
 };
+
+function createAllOldRedirects(createRedirect) {
+  function redirect(from, to) {
+    if (from.length === 0 || to.length === 0) {
+      throw new Error('wrong redirect');
+    }
+    createRedirect({fromPath: from, toPath: to, isPermanent: true});
+  }
+
+  redirect('/blog/tag/hebrew', '/posts/hebrew');
+  redirect('/blog/tag/specs2', '/posts/specs2');
+  redirect('/blog/tag/wix', '/posts/wix');
+  redirect('/single-post/2015/07/14/About-Ulpan', '/p/about-ulpan/');
+  redirect('/2015/07/15/about-ulpan.html', '/p/about-ulpan/');
+  redirect('/2015/09/10/small-design-issues.html', '/p/small-design-issues/');
+  redirect('/single-post/2015/09/15/Testing-asynchronous-code', '/p/testing-asynchronous-code/');
+  redirect('/2015/09/13/testing-asynchronous-code.html', '/p/testing-asynchronous-code/');
+  redirect('/2016/12/03/scala-stringbuilder-vs-java-stringbuilder-performance.html', '/p/scala-stringbuilder-vs-java-stringbuilder-performance/');
+  redirect('/2016/01/09/scala-how-to-return-a-result-code-in-a-concise-way.html', '/p/scala-how-to-return-a-result-code-in-a-concise-way/');
+  redirect('/2015/11/05/micro-optimization-for-uuid-fromstring-in-7-steps.html', '/p/micro-optimization-for-uuid-fromstring-in-7-steps/');
+  redirect('/2016/12/05/scala-string-interpolation-performance.html', '/p/scala-string-interpolation-performance/');
+  redirect('/2015/10/12/first-time-on-wix-engineering-blog.html', '/p/first-time-on-wix-engineering-blog/');
+  redirect('/2015/11/16/lets-continue-with-uuid-fromstring.html', '/p/lets-continue-with-uuid-fromstring/');
+  redirect('/2016/06/26/scala-serialization-updated.html', '/p/scala-serialization-updated/');
+  redirect('/2016/02/02/one-more-threat-of-mockito.html', '/p/one-more-threat-of-mockito/');
+  redirect('/2016/06/12/scala-serialization.html', '/p/scala-serialization/');
+}
