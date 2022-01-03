d6cb03d3f0d093cbed5660dc8a58ac7ef46a2b95
Dmitry Komanov <dmitryk@wix.com>
2022-01-03 23:01:47 +0200

    Reuse Article in BlogPostList

--- src/components/BlogPostList/BlogPostList.js
+++ src/components/BlogPostList/BlogPostList.js
@@ -1,39 +1,39 @@
-import {Link} from 'gatsby';
+import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import './BlogPostList.css';
+import { Article } from '..';
 import Pagination from './Pagination/Pagination';
 import Tags from './Tags/Tags';
 
 class BlogPostPreview extends React.Component {
   static propTypes = {
-    node: PropTypes.object.isRequired
+    node: PropTypes.object.isRequired,
   };
 
   render() {
-    const {node} = this.props;
+    const { node } = this.props;
 
     const slug = node.fields.slug;
     const title = node.frontmatter.title || slug;
     const date = node.frontmatter.date;
 
     return (
-      <article className="post" itemScope itemType="http://schema.org/BlogPosting">
-        <div className="article-item">
-          <header className="post-header">
-            <h2 className="post-title" itemProp="name">
-              <Link to={slug} itemProp="url">{title}</Link>
-            </h2>
-          </header>
-          <section className="post-description" itemProp="description">
-            <p dangerouslySetInnerHTML={{__html: node.frontmatter.description || node.excerpt}}/>
-          </section>
-          <div className="post-meta">
-            <time dateTime={date}>{date}</time>
-            <Tags tags={node.frontmatter.tags}/>
-          </div>
-        </div>
-      </article>
+      <Article
+        header={
+          <Link to={slug} itemProp="url">
+            {title}
+          </Link>
+        }
+        content={
+          <p
+            dangerouslySetInnerHTML={{
+              __html: node.frontmatter.description || node.excerpt,
+            }}
+          />
+        }
+        date={date}
+        meta={<Tags tags={node.frontmatter.tags} />}
+      />
     );
   }
 }
@@ -43,11 +43,11 @@ export default class BlogPostList extends React.Component {
     posts: PropTypes.array.isRequired,
     page: PropTypes.number.isRequired,
     pageSize: PropTypes.number.isRequired,
-    urlFunc: PropTypes.func.isRequired
+    urlFunc: PropTypes.func.isRequired,
   };
 
   render() {
-    const {page, pageSize, urlFunc, posts} = this.props;
+    const { page, pageSize, urlFunc, posts } = this.props;
     const first = (page - 1) * pageSize;
 
     if (first < 0 || first >= posts.length) {
@@ -56,12 +56,15 @@ export default class BlogPostList extends React.Component {
 
     return (
       <div>
-        {
-          posts
-            .slice(first, first + pageSize)
-            .map(post => <BlogPostPreview key={post.fields.slug} node={post}/>)
-        }
-        <Pagination pageSize={pageSize} totalCount={posts.length} page={page} urlFunc={urlFunc}/>
+        {posts.slice(first, first + pageSize).map((post) => (
+          <BlogPostPreview key={post.fields.slug} node={post} />
+        ))}
+        <Pagination
+          pageSize={pageSize}
+          totalCount={posts.length}
+          page={page}
+          urlFunc={urlFunc}
+        />
       </div>
     );
   }
